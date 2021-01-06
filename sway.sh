108523eeef6489a6e7a229d0423ddee78b727aa7
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2021-01-06 22:49:12 +0530

    include xwayland | made stand-alone

---
+++ sway.sh
@@ -0,0 +1,8 @@
+sudo pacman -S sway termite wofi waybar mpv alsa-utils pcmanfm-gtk3 gvfs file-roller arc-gtk-theme arc-icon-theme torbrowser-launcher xorg-xwayland ttf-jetbrains-mono ttf-nerd-fonts-symbols --needed --noconfirm
+
+chmod +x .config/wofi/* 
+
+cp -r .config /home/user/
+
+sway
+
