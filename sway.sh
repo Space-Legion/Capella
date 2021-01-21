9e298463681544fe0c0db2381f38af8ca6aacba5
yperta <62870678+yperta@users.noreply.github.com>
2021-01-21 16:37:19 +0530

    all def to functions | menu updated

--- sway.sh
+++ sway.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+base() {
 sudo pacman -S sway termite wofi waybar mpv alsa-utils pcmanfm-gtk3 swaylock gvfs file-roller eog arc-gtk-theme arc-icon-theme torbrowser-launcher xorg-xwayland ttf-jetbrains-mono ttf-nerd-fonts-symbols --needed --noconfirm
 
 chmod +x .config/wofi/* 
@@ -8,38 +11,33 @@ curl https://raw.githubusercontent.com/Space-Legion/assets/main/moon.jpg > /$HOM
 
 curl https://raw.githubusercontent.com/Space-Legion/assets/main/space-shuttle.jpg > /$HOME/.config/l.jpg
 
-x=0
-while [[ $x == 0 ]]
-
-do
-	clear
-        echo "
-
-         Do you wish to setup the firewall now? [y/n]
-"
-	read answer
-	
-	case "$answer" in 
-
- 		y)
-        chmod +x cloak-iptables && sudo ./cloak-iptables && cd && sway
-        
-		x=1
-		;;
-
-		n)
-        cd && sway
-        
-		x=1
-		;;
-
-        *)
-        printf "\nInvalid Option" & sleep 3 && exit 0
-
-        ;;
-		
-
-	esac	
-done
+}
 
 
+firewall() { 
+clear
+read -n 1 -p "
+
+ Do you wish to setup the firewall now? [y/n] 
+
+" ans;
+case $ans in
+
+    Y|y)
+
+    chmod +x cloak-iptables && sudo ./cloak-iptables && cd && sway;;
+
+
+    n|N)
+       cd && sway;;
+
+    *)
+     firewall 
+
+esac
+}
+
+
+#functions call 
+base
+firewall
